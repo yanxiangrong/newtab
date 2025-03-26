b30751ed635b74d61769c10ac34bc9ded05a15f7
严相荣 <yanxiangrong@huya.com>
2025-03-26 15:47:34 +0800

    1

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,18 @@
 import './assets/main.css'
 
-import { createApp } from 'vue'
+import {createApp} from 'vue'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
 import App from './App.vue'
 import router from './router'
+import zhCn from 'element-plus/es/locale/lang/zh-cn'
+
 
 const app = createApp(App)
 
+app.use(ElementPlus, {
+    locale: zhCn
+})
 app.use(router)
 
 app.mount('#app')
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,46 @@
 <script setup lang="ts">
 import TheWelcome from '../components/TheWelcome.vue'
+import {ref} from "vue";
+
+
+const bookmarks = ref<chrome.bookmarks.BookmarkTreeNode[]>();
+
+if (chrome.bookmarks) {
+  chrome.bookmarks.getTree((tree) => {
+    if (!tree || !tree[0].children) {
+      return
+    }
+
+    for (const node of tree[0].children) {
+      if (!node.folderType && node.children) {
+        bookmarks.value = node.children
+        break
+      }
+      if (node.folderType === "bookmarks-bar" && node.children) {
+        bookmarks.value = node.children
+        break
+      }
+    }
+
+    console.log('chrome.bookmarks:', bookmarks.value)
+  })
+} else {
+  console.log('chrome.bookmarks is not available')
+}
+
+
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <TheWelcome/>
+    <section>
+      <h2>Bookmarks</h2>
+      <ul>
+        <li v-for="bookmark in bookmarks" :key="bookmark.id">
+          <a :href="bookmark.url" target="_blank">{{ bookmark.title }}</a>
+        </li>
+      </ul>
+    </section>
   </main>
 </template>
