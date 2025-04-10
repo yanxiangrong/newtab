d0732025c647aaf3cf9d76990329638a80758cad
严相荣 <yanxiangrong@huya.com>
2025-04-10 18:38:47 +0800

    1

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -13,38 +13,25 @@ const {searchEngine, searchEngines} = storeToRefs(configStore)
 const searchHistoryNum = 1000
 const searchHistory = ref<string[]>([])
 
-const getSearchHistory = async () => {
-  const searchHistoryAsync = (queryText: string): Promise<{ query: string, time: number }[]> => {
-    return new Promise((resolve) => {
-      let history = <{ query: string, time: number }[]>([])
-      chrome.history.search({text: queryText, maxResults: searchHistoryNum}, (historyItems) => {
-        const paramsKeys = ['wd', 'q', 'query', 'search', 's']
 
-        for (const historyItem of historyItems) {
-          if (!historyItem.url) continue
-          const urlObj = new URL(historyItem.url)
-
-          let query = ''
-
-          for (const key of paramsKeys) {
-            if (urlObj.searchParams.has(key)) {
-              query = urlObj.searchParams.get(key) || ''
-              break
-            }
-          }
-
-          if (!query) continue
-
-          history.push({
-            query: query,
-            time: historyItem.lastVisitTime ? historyItem.lastVisitTime : 0
-          })
-        }
-
-        resolve(history)
-      })
+const searchChromeHistoryAsync = (queryText: string, maxResults: number): Promise<chrome.history.HistoryItem[]> => {
+  return new Promise((resolve) => {
+    chrome.history.search({text: queryText, maxResults: maxResults}, (historyItems) => {
+      resolve(historyItems)
     })
-  }
+  })
+}
+
+const searchChromeBookmarkAsync = (queryText: string, maxResults: number): Promise<chrome.bookmarks.BookmarkTreeNode[]> => {
+  return new Promise((resolve) => {
+    chrome.bookmarks.search(queryText, (bookmarks) => {
+      resolve(bookmarks)
+    })
+  })
+}
+
+const getSearchHistory = async () => {
+  const paramsKeys = ['wd', 'q', 'query', 'search', 's']
 
   let chromeSearchHistory = <{ query: string, time: number }[]>([])
   for (const engine of searchEngines.value) {
@@ -52,7 +39,26 @@ const getSearchHistory = async () => {
     const engineUrl = new URL(engine.value)
     const queryText = engineUrl.origin
 
-    const history = await searchHistoryAsync(queryText)
+    let history = <{ query: string, time: number }[]>([])
+
+    const historyItems = await searchChromeHistoryAsync(queryText, searchHistoryNum)
+    for (const historyItem of historyItems) {
+      if (!historyItem.url) continue
+      const urlObj = new URL(historyItem.url)
+
+      for (const key of paramsKeys) {
+        if (!urlObj.searchParams.has(key)) continue
+
+        let query = urlObj.searchParams.get(key) || ''
+        if (query) {
+          history.push({
+            query: query,
+            time: historyItem.lastVisitTime || 0
+          })
+          break
+        }
+      }
+    }
     chromeSearchHistory.push(...history)
   }
 
@@ -68,13 +74,13 @@ const getSearchHistory = async () => {
     searchHistory.push(historyItem.query)
   }
 
-
   return searchHistory
 }
 
 (async () => {
   if (!chrome || !chrome.history) {
     searchHistory.value = JSON.parse(localStorage.getItem("searchHistory") || "[]")
+    watch(searchHistory, () => localStorage.setItem("searchHistory", JSON.stringify(searchHistory.value)))
     return
   }
   searchHistory.value = await getSearchHistory()
@@ -91,13 +97,13 @@ const openSearch = (query: string) => {
   window.location.assign(s.value + query)
 }
 
-const fetchSearchSuggestions = async (query: string) => {
+const fetchSearchSuggestions = async (query: string): Promise<string[]> => {
   const api = new URL('https://suggestion.baidu.com/su')
   api.searchParams.set('wd', query)
   api.searchParams.set('action', 'opensearch')
 
   const response = await fetch(api)
-  if (!response.ok) return
+  if (!response.ok) return []
   // 获取响应头中的 Content-Type
   const contentType = response.headers.get('Content-Type') || ''
 
@@ -114,37 +120,60 @@ const fetchSearchSuggestions = async (query: string) => {
   const decodedText = new TextDecoder(charset).decode(buffer)
 
   const data = JSON.parse(decodedText)
-  if (!data || !data[1]) return
+  if (!data || !data[1]) return []
   return data[1]
 }
 
+interface SuggestionItem {
+  value: string
+  link: string | null
+}
+
 const querySearch = async (query: string, cb: (suggestions: object[]) => void) => {
-  let suggestions: string[] = []
+  let suggestions: SuggestionItem[] = []
   const suggestionNum = 25
 
-  if (query) {
-    for (const q of searchHistory.value) {
-      if (q.startsWith(query)) suggestions.push(q)
-      if (suggestions.length >= suggestionNum) break
-    }
+  if (!query) {
+    suggestions = searchHistory.value.slice(0, suggestionNum).map(item => {
+      return {value: item, link: null}
+    })
   } else {
-    suggestions = searchHistory.value.slice(0, suggestionNum)
-  }
+    // 书签
+    if (chrome && chrome.bookmarks) {
+      const bookmarks = await chrome.bookmarks.search(query)
+      suggestions.push(...bookmarks.map(item => {
+        return {value: item.title || '', link: item.url || null}
+      }).slice(0, suggestionNum - suggestions.length))
+    }
 
-  if (suggestions.length < suggestionNum) {
-    try {
-      const searchSuggestions = await fetchSearchSuggestions(query)
-      if (searchSuggestions) {
-        suggestions.push(...searchSuggestions.slice(0, suggestionNum - suggestions.length))
+    // 搜索历史
+    if (suggestions.length < suggestionNum) {
+      suggestions.push(...searchHistory.value.filter(q => q.includes(query)).slice(0, suggestionNum - suggestions.length).map(item => {
+        return {value: item, link: null}
+      }))
+    }
+
+    // Chrome 历史记录
+    if (suggestions.length < suggestionNum && chrome && chrome.history) {
+      const historyItems = await searchChromeHistoryAsync(query, suggestionNum - suggestions.length)
+      suggestions.push(...historyItems.map(item => {
+        return {value: item.title || '', link: item.url || null}
+      }))
+    }
+
+    // 搜索建议
+    if (suggestions.length < suggestionNum) {
+      try {
+        const searchSuggestions = await fetchSearchSuggestions(query)
+        suggestions.push(...searchSuggestions.slice(0, suggestionNum - suggestions.length).map(item => {
+          return {value: item, link: null}
+        }))
+      } catch (e) {
       }
-    } catch (e) {
     }
   }
 
-  let results = suggestions.map((suggestion) => {
-    return {value: suggestion}
-  })
-  cb(results)
+  cb(suggestions)
 }
 
 
