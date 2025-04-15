9e26d791d0d7907ce688b325919c2c71d3fb36e0
严相荣 <yanxiangrong@huya.com>
2025-04-15 16:46:27 +0800

    1

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -182,7 +182,7 @@ const handleSuggestionSelect = (item: SuggestionItem) => {
 
 <template>
   <el-autocomplete ref="autoComplete" style="max-width: 750px" v-model="input" autofocus size="large" @keydown.enter="openSearch(input)"
-                   :fetch-suggestions="querySearch" @select="handleSuggestionSelect">
+                   :fetch-suggestions="querySearch" @select="handleSuggestionSelect" class="search-input">
     <template #prepend>
       <el-select style="width: 115px" v-model="searchEngine" size="large">
         <el-option v-for="engine in searchEngines" :key="engine.label" :label="engine.label"
@@ -241,4 +241,12 @@ const handleSuggestionSelect = (item: SuggestionItem) => {
   text-overflow: ellipsis;
 }
 
+
+</style>
+
+<style>
+.search-input .el-input--large {
+  font-size: 16px;
+  font-weight: 600;
+}
 </style>
