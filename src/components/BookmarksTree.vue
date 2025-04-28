8f31661c5e09ebdc4dd38b779d68567695293363
严相荣 <yanxiangrong@huya.com>
2025-04-28 11:04:06 +0800

    refactor: enhance bookmarks tree component with dropdown and favicon support

--- src/components/BookmarksTree.vue
+++ src/components/BookmarksTree.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import {Folder} from "@element-plus/icons-vue";
-import BookmarksItem from "@/components/BookmarksItem.vue";
+import {faviconURL} from "@/utils/utils.ts";
 
 defineProps<{
   nodes: chrome.bookmarks.BookmarkTreeNode[],
@@ -9,23 +9,41 @@ defineProps<{
 </script>
 
 <template>
-  <template v-for="node in nodes" :key="node.id">
-    <el-sub-menu class="sub-menu" v-if="node.children" :index="node.id">
-      <template #title>
+  <li v-for="node in nodes" class="bookmarks-wrap" :key="node.id">
+    <el-dropdown v-if="node.children" :index="node.id">
+      <div class="bookmarks-text">
         <el-icon class="favicon" v-if="node.title">
           <Folder/>
         </el-icon>
-        {{ node.title }}
-      </template>
-      <div class="scroll">
-        <bookmarks-tree :nodes="node.children"/>
+        <span class="bookmark-span">{{ node.title }}</span>
       </div>
-    </el-sub-menu>
-    <bookmarks-item v-else :title="node.title" :url="node.url || ''"/>
-  </template>
+      <template #dropdown>
+        <el-dropdown-menu>
+          <bookmarks-tree :nodes="node.children"/>
+        </el-dropdown-menu>
+      </template>
+    </el-dropdown>
+    <el-link v-else :underline="false" :href="node.url">
+      <div class="bookmarks-text">
+        <el-icon class="favicon" v-if="node.url">
+          <img loading="lazy" :src="faviconURL(node.url)" alt=""/>
+        </el-icon>
+        <span class="bookmark-span">
+        {{ node.title }}
+        </span>
+      </div>
+    </el-link>
+  </li>
 </template>
 
 <style scoped>
+.bookmarks-wrap {
+  display: flex;
+  align-items: center;
+  height: 100%;
+  margin: 0 2px;
+}
+
 .favicon {
   font-size: 16px;
   margin-right: 4px;
@@ -36,5 +54,46 @@ defineProps<{
 .scroll {
   max-height: calc(100vh - 20px);
   overflow-y: auto;
+  display: flex;
+  flex-direction: column;
+  list-style: none;
+  padding: 0;
+}
+
+.bookmarks-text {
+  display: inline-flex;
+  align-items: center;
+  flex-wrap: nowrap;
+  text-wrap: nowrap;
+  box-sizing: border-box;
+  color: var(--el-menu-text-color);
+  height: 24px;
+  padding: 8px;
+  transition: background-color 0.3s ease;
+  background-color: rgba(255, 255, 255, 0);
+  border-radius: 12px;
+}
+
+.bookmarks-text:hover,
+.bookmarks-text:hover:focus,
+.bookmarks-text:hover:focus-visible {
+  background-color: rgba(255, 255, 255, 0.3);
+}
+
+.bookmark-span {
+  max-width: 10em;
+  overflow: hidden;
+  white-space: nowrap;
+  text-overflow: ellipsis;
+}
+
+.el-link:hover {
+  color: inherit !important;
+  text-decoration: none !important;
+  background: none !important;
+}
+
+.el-tooltip__trigger:focus-visible{
+  outline: none !important;
 }
 </style>
