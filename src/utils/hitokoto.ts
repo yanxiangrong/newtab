b577d650328ad2386a12e350ec2f411b823f7d1b
严相荣 <yanxiangrong@huya.com>
2025-05-06 18:21:44 +0800

    feat: enhance Hitokoto data structure with type field and improve history management

--- src/utils/hitokoto.ts
+++ src/utils/hitokoto.ts
@@ -2,6 +2,7 @@ import {watch} from "vue";
 
 export type Hitokoto = {
     content: string;
+    type: string;
     from: string;
     href: string
 }
@@ -25,18 +26,22 @@ const bufferKey = 'hitokotoHistory';
 const bufferSize = 1000;
 
 export const hitokotoHistory = ref<Hitokoto[]>(JSON.parse(localStorage.getItem(bufferKey) || "[]"))
-watch(hitokotoHistory, () => localStorage.setItem(bufferKey, JSON.stringify(hitokotoHistory.value)))
+watch(hitokotoHistory, () => localStorage.setItem(bufferKey, JSON.stringify(hitokotoHistory.value)), {deep: true})
 
 const updateHitokotoHistory = (hitokoto: Hitokoto) => {
+    if (hitokotoHistory.value.some(item => item.content === hitokoto.content)) {
+        return
+    }
     while (hitokotoHistory.value.length >= bufferSize) {
         hitokotoHistory.value.shift();
     }
     hitokotoHistory.value.push(hitokoto);
+
 }
 
 const getHitokotoFromHistoryRand = (types: string[] = []): Hitokoto | null => {
     const filteredHistory = hitokotoHistory.value.filter(hitokoto => {
-        return types.length === 0 || types.includes(hitokoto.from);
+        return types.length === 0 || types.includes(hitokoto.type);
     });
     if (filteredHistory.length === 0) {
         return null;
@@ -51,19 +56,22 @@ export async function getHitokoto(types: string[] = []): Promise<Hitokoto> {
     url.searchParams.set('charset', 'utf-8')
     types.forEach(type => url.searchParams.append('c', type));
 
-    const response = await fetch(url);
+    const response = await fetch(url, {mode: 'no-cors'});
     if (!response.ok) {
         const hitokoto = getHitokotoFromHistoryRand(types);
         if (hitokoto) {
+            console.warn('Hitokoto API request failed, using cached hitokoto:', hitokoto);
             return hitokoto;
         }
+        console.error('Hitokoto API request failed, no cached hitokoto available');
         throw new Error('Failed to fetch Hitokoto');
     }
     const data = await response.json();
     const hitokoto = {
         content: data.hitokoto,
         from: data.from || data.from_who || data.creator || '一言',
-        href: `https://hitokoto.cn/?uuid=${data.uuid}`
+        href: `https://hitokoto.cn/?uuid=${data.uuid}`,
+        type: data.type
     }
 
     updateHitokotoHistory(hitokoto);
